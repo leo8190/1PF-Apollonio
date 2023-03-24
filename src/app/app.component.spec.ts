b479f3d8675763527edcdeb051ab403157b5814c
Leo <Leo@DESKTOP-FCMPPV6>
2023-03-24 17:42:15 -0300

    Project name changed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title '2PF-Apollonio'`, () => {
+  it(`should have as title 'PF-Apollonio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('2PF-Apollonio');
+    expect(app.title).toEqual('PF-Apollonio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('2PF-Apollonio app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('PF-Apollonio app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '2PF-Apollonio';
+  title = 'PF-Apollonio';
 }
